5dcc9f5f41b181be1b98f424f5523daa4cf9991d
Ashley Evans <hoshy@hoshy.co.uk>
2012-08-27 13:22:54 +0200

    Initial commit

---
+++ Working Hours Inline Functions/Working Hours Inline Functions.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Design;
+using System.Text;
+
+using SourceCode.Framework;
+using SourceCode.Framework.Design;
+using System.Security.Cryptography;
+using SourceCode.Workflow.Management;
+
+using System.Linq;
+
+namespace K2NE
+{
+    /// <summary>
+    /// A class comprising functions.
+    /// </summary>
+    // Must be a public class.
+    [Category("K2NE.WorkingHoursInlineFunctions.Properties.Resources", "CategoryName", typeof(WorkingHoursInlineFunctions))]
+    public class WorkingHoursInlineFunctions
+    {
+        /*
+         * To register the custom inline function with the K2 platform copy the assembly to the following folder on all K2 Servers:
+         *   C:\Program Files\K2 blackpearl (x86)\Host Server\BIN
+         * 
+         * To register the custom inline function with the K2 client side design tools, copy the assembly to the following folder on all K2
+         * development workstations:
+         *   C:\Program Files\K2 blackpearl (x86)\BIN\Functions
+         * 
+         * Note: K2 Install path is usually: "C:\Program Files\K2 blackpearl (x86)"
+         ***********************************************************************************************************************************
+         * To register the custom inline function with the K2 Designer for SharePoint design tool, ensure the assembly has a strong name and
+         * then GAC the assembly on all K2 Servers:
+         *   "C:\Program Files\Microsoft SDKs\Windows\v6.0A\bin\gacutil" /if "CustomInlineFunction_Sample.dll"
+         * 
+         * Copy the icon file to:
+         *   "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\12\TEMPLATE\LAYOUTS\WebDesigner\Images\Functions"
+         * 
+         * Note: SharePoint Web Server Extensions install path is usually: "C:\Program Files (x86)\Common Files\Microsoft Shared\Web Server Extensions"
+         */
+
+        #region Double WorkingHoursDateDiffAsMinutes(string zoneName, DateTime startDate, DateTime endDate)
+        [DisplayName("K2NE.WorkingHoursInlineFunctions.Properties.Resources", "DateDiffName", typeof(WorkingHoursInlineFunctions))]
+        [Description("K2NE.WorkingHoursInlineFunctions.Properties.Resources", "DateDiffDescription", typeof(WorkingHoursInlineFunctions))]
+        [K2Icon(typeof(WorkingHoursInlineFunctions), "Resources.Icon.png")]
+        public static Double WorkingHoursDateDiffAsMinutes(
+            [DisplayName("K2NE.WorkingHoursInlineFunctions.Properties.Resources", "DateDiffZoneName", typeof(WorkingHoursInlineFunctions))]
+            [Description("K2NE.WorkingHoursInlineFunctions.Properties.Resources", "DateDiffZoneDescription", typeof(WorkingHoursInlineFunctions))]
+            string zoneName,
+            [DisplayName("K2NE.WorkingHoursInlineFunctions.Properties.Resources", "DateDiffStartDateName", typeof(WorkingHoursInlineFunctions))]
+            [Description("K2NE.WorkingHoursInlineFunctions.Properties.Resources", "DateDiffStartDateDescription", typeof(WorkingHoursInlineFunctions))]
+            DateTime startDate,
+            [DisplayName("K2NE.WorkingHoursInlineFunctions.Properties.Resources", "DateDiffEndDateName", typeof(WorkingHoursInlineFunctions))]
+            [Description("K2NE.WorkingHoursInlineFunctions.Properties.Resources", "DateDiffEndDateDescription", typeof(WorkingHoursInlineFunctions))]
+            DateTime endDate)
+        {
+            return WorkingHoursDateDiff(zoneName, startDate, endDate).TotalMinutes;        
+        }
+        #endregion
+
+        private static TimeSpan WorkingHoursDateDiff(string zoneName, DateTime startDate, DateTime endDate)
+        {
+            TimeSpan workedHours = new TimeSpan(0);
+
+            WorkflowManagementServer mngServer = new WorkflowManagementServer("localhost", 5555); //safe to do this as inline function runs on K2 Server.
+            mngServer.Open(); //let K2 catch errors here.
+
+            AvailabilityZone zone = new AvailabilityZone();
+
+            if (string.IsNullOrEmpty(zoneName)) //use default working hours if not specified.
+            {
+                try
+                {
+                    zoneName = mngServer.ZoneGetDefault();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Couldn't get default working hours because: " + ex.Message, ex);
+                }
+            }
+
+            try
+            {
+                zone = mngServer.ZoneLoad(zoneName);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Couldn't get working hours named: '" + zoneName + "' Because: " + ex.Message, ex);
+            }
+
+            DateTime currentZoneDate = startDate.Date;
+            bool carryOn = true;
+            while (carryOn)
+            {
+
+                List<AvailabilityHours> singleDayListOfHours = zone.AvailabilityHoursList.Where(x => x.WorkDay == currentZoneDate.DayOfWeek).ToList();
+
+                foreach (AvailabilityHours hours in singleDayListOfHours)
+                {
+                    //quickly filter out exception dates :)
+                    if (zone.AvailabilityDateList.Count(x => x.IsNonWorkDate == true && x.WorkDate.Date == currentZoneDate.Date) > 0)
+                        continue;
+                    DateTime endOfSection = new DateTime(currentZoneDate.Year, currentZoneDate.Month, currentZoneDate.Day);
+                    DateTime startOfSection = new DateTime(currentZoneDate.Year, currentZoneDate.Month, currentZoneDate.Day);
+                    startOfSection = startOfSection.Add(hours.TimeOfDay);
+                    endOfSection = startOfSection.Add(hours.Duration);
+
+                    if ((startDate >= startOfSection && startDate <= endOfSection) && (endDate <= endOfSection && endDate >= startOfSection)) //starts inside, ends inside
+                    {
+                        workedHours += endDate - startDate;
+                    }
+
+                    if (startDate < startOfSection && endDate > endOfSection) // starts outside, ends outside
+                    {
+                        workedHours += endOfSection - startOfSection;
+                    }
+
+                    if (startDate < startOfSection && (endDate <= endOfSection && endDate >= startOfSection)) //starts outside, ends inside
+                    {
+                        workedHours += endDate - startOfSection;
+                    }
+
+                    if ((startDate >= startOfSection && startDate <= endOfSection) && endDate > endOfSection) //starts inside, ends outside
+                    {
+                        workedHours += endOfSection - startDate;
+                    }
+                }
+
+                //special dates require more work - basically duplicate the logic above but decrement worked hours for every special day
+                foreach (AvailabilityDate specialDate in zone.AvailabilityDateList.Where(x => x.WorkDate.Date == currentZoneDate.Date && x.IsNonWorkDate == false))
+                {
+
+                    DateTime specialDateStart = specialDate.WorkDate;
+                    DateTime specialDateEnd = specialDate.WorkDate + specialDate.Duration;
+
+                    if ((startDate >= specialDateStart && startDate <= specialDateEnd) && (endDate <= specialDateEnd && endDate >= specialDateStart)) //starts inside, ends inside
+                    {
+                        workedHours -= endDate - startDate;
+                    }
+
+                    if (startDate < specialDateStart && endDate > specialDateEnd) // starts outside, ends outside
+                    {
+                        workedHours -= specialDateEnd - specialDateStart;
+                    }
+
+                    if (startDate < specialDateStart && (endDate <= specialDateEnd && endDate >= specialDateStart)) //starts outside, ends inside
+                    {
+                        workedHours -= endDate - specialDateStart;
+                    }
+
+                    if ((startDate >= specialDateStart && startDate <= specialDateEnd) && endDate > specialDateEnd) //starts inside, ends outside
+                    {
+                        workedHours -= specialDateEnd - startDate;
+                    }
+                }
+
+                if (currentZoneDate > endDate)
+                    carryOn = false;
+
+                currentZoneDate = currentZoneDate.AddDays(1);
+
+            }
+
+            mngServer.Connection.Close();
+            mngServer.Connection.Dispose();
+
+            return workedHours;
+        }
+    }
+}
---
+++ WorkingHoursInlineFunctionsTest/WorkingHoursTests.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SourceCode.Workflow.Management;
+using K2NE;
+
+namespace WorkingHoursInlineFunctionsTest
+{
+    /// <summary>
+    /// *** This require that it's run on a K2 Server machine.
+    /// *** This requries the a set of working hours as follows:
+    /// *** Name: Test Working Hours
+    /// *** Monday - Friday, 9am - 5pm, excluding 1 hour lunch from 12pm - 1pm
+    /// *** Exception Dates: 
+    ///         December 26th 2011 (25th is a Sunday!)
+    ///         Jan 2nd 2012 
+    /// *** Special Dates: 
+    ///         June 30th 2011 9am - 12pm
+    ///         March 11th 2011 9am - 12pm
+    /// </summary>
+    [TestClass]
+    public class TestInlineFunctions
+    {
+        private string workingHoursZoneName = "Test Working Hours";
+
+        public TestInlineFunctions()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void DateDiffTestCase()
+        {
+
+            //TODO: Refactor to seperate test methods.
+
+            ValidateWorkingHours();
+
+            DateTime startDate = new DateTime();
+            DateTime endDate = new DateTime();
+            Double result = new Double();
+
+            //starts inside, ends outside - crosses multiple sections
+            startDate = new DateTime(2011, 03, 14, 10, 00, 00);
+            endDate = new DateTime(2011, 03, 14, 19, 00, 00);
+            result = WorkingHoursInlineFunctions.WorkingHoursDateDiffAsMinutes(workingHoursZoneName, startDate, endDate);
+            Assert.AreEqual(new TimeSpan(6, 0, 0).TotalMinutes, result);
+
+            //starts outside, ends inside - crossing multiple sections
+            startDate = new DateTime(2011,03,14,08,00,00);
+            endDate = new DateTime(2011,03,14,15,00,00);
+            result = WorkingHoursInlineFunctions.WorkingHoursDateDiffAsMinutes(workingHoursZoneName, startDate, endDate);
+            Assert.AreEqual(new TimeSpan(5, 0, 0).TotalMinutes, result);
+
+            //starts inside, ends inside - crossing multiple sections
+            startDate = new DateTime(2011, 03, 14, 10, 00, 00);
+            endDate = new DateTime(2011, 03, 14, 15, 00, 00);
+            result = WorkingHoursInlineFunctions.WorkingHoursDateDiffAsMinutes(workingHoursZoneName, startDate, endDate);
+            Assert.AreEqual(new TimeSpan(4, 0, 0).TotalMinutes, result);
+
+            //one exception day
+            startDate = new DateTime(2011, 12, 26, 09, 00, 00);
+            endDate = new DateTime(2011, 12, 26, 17, 00, 00);
+            result = WorkingHoursInlineFunctions.WorkingHoursDateDiffAsMinutes(workingHoursZoneName, startDate, endDate);
+            Assert.AreEqual(new TimeSpan(0, 0, 0).TotalMinutes, result);
+
+            //starts inside, ends inside - crossing multiple sections and one exception day and one weekend day
+            startDate = new DateTime(2011, 12, 26, 09, 00, 00);
+            endDate = new DateTime(2011, 12, 27, 17, 00, 00);
+            result = WorkingHoursInlineFunctions.WorkingHoursDateDiffAsMinutes(workingHoursZoneName, startDate, endDate);
+            Assert.AreEqual(new TimeSpan(7, 0, 0).TotalMinutes, result);
+
+            //crosses year boundry, starts outside, ends outside, crosses exception date
+            startDate = new DateTime(2011, 12, 31, 09, 00, 00);
+            endDate = new DateTime(2012, 01, 03, 19, 00, 00);
+            result = WorkingHoursInlineFunctions.WorkingHoursDateDiffAsMinutes(workingHoursZoneName, startDate, endDate);
+            Assert.AreEqual(new TimeSpan(7, 0, 0).TotalMinutes, result);
+           
+        }
+
+        /// <summary>
+        /// Check that the local K2 Server has a valid set of working hours for this test.
+        /// </summary>
+        private void ValidateWorkingHours()
+        {
+
+            WorkflowManagementServer mngServer = new WorkflowManagementServer("localhost", 5555);
+            mngServer.Open();
+            AvailabilityZone zone = mngServer.ZoneLoad(workingHoursZoneName);
+
+            List<AvailabilityDate> specialDates = zone.AvailabilityDateList.Where(x => x.IsNonWorkDate == false).ToList();
+            if (specialDates.Count > 2)
+                Assert.Fail("Too many special dates");
+
+            List<AvailabilityDate> exceptionDates = zone.AvailabilityDateList.Where(x => x.IsNonWorkDate == true).ToList();
+            if (exceptionDates.Count > 2)
+                Assert.Fail("Too many exception dates");
+
+            if (specialDates.Where(x => x.WorkDate == new DateTime(2011, 06, 30, 09, 00, 00) && x.Duration == new TimeSpan(3, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem finding special date June 30th 2011 09:00 - 12:00");
+
+            if (specialDates.Where(x => x.WorkDate == new DateTime(2011, 03, 11, 09, 00, 00) && x.Duration == new TimeSpan(3, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem finding special date March 11th 2011 09:00 - 12:00");
+
+            if (exceptionDates.Where(x => x.WorkDate == new DateTime(2011, 12, 26)).ToList().Count != 1)
+                Assert.Fail("Problem finding exception date December 26th 2011");
+
+            if (exceptionDates.Where(x => x.WorkDate == new DateTime(2012, 1, 2)).ToList().Count != 1)
+                Assert.Fail("Problem finding exception date Jan 2nd 2012");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Monday && x.Duration == new TimeSpan(3, 0, 0) && x.TimeOfDay == new TimeSpan(9, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem with Monday 9am - 12pm working hours");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Tuesday && x.Duration == new TimeSpan(3, 0, 0) && x.TimeOfDay == new TimeSpan(9, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem with Tuesday 9am - 12pm working hours");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Wednesday && x.Duration == new TimeSpan(3, 0, 0) && x.TimeOfDay == new TimeSpan(9, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem with Wednesday 9am - 12pm working hours");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Thursday && x.Duration == new TimeSpan(3, 0, 0) && x.TimeOfDay == new TimeSpan(9, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem with Thursday 9am - 12pm working hours");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Friday && x.Duration == new TimeSpan(3, 0, 0) && x.TimeOfDay == new TimeSpan(9, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem with Friday 9am - 12pm working hours");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Monday && x.Duration == new TimeSpan(4, 0, 0) && x.TimeOfDay == new TimeSpan(13, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem with Monday 1pm - 5pm working hours");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Tuesday && x.Duration == new TimeSpan(4, 0, 0) && x.TimeOfDay == new TimeSpan(13, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem with Tuesday 1pm - 5pm working hours");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Wednesday && x.Duration == new TimeSpan(4, 0, 0) && x.TimeOfDay == new TimeSpan(13, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem with Wednesday 1pm - 5pm working hours");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Thursday && x.Duration == new TimeSpan(4, 0, 0) && x.TimeOfDay == new TimeSpan(13, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem with Thursday 1pm - 5pm working hours");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Friday && x.Duration == new TimeSpan(4, 0, 0) && x.TimeOfDay == new TimeSpan(13, 0, 0)).ToList().Count != 1)
+                Assert.Fail("Problem with Friday 1pm - 5pm working hours");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Saturday).ToList().Count > 0)
+                Assert.Fail("Found working hours on Saturday where there should be none");
+
+            if (zone.AvailabilityHoursList.Where(x => x.WorkDay == DayOfWeek.Sunday).ToList().Count > 0)
+                Assert.Fail("Found working hours on Sunday wherer there should be none");
+
+            mngServer.Connection.Close();
+            mngServer.Connection.Dispose();
+
+        }
+    }
+}
